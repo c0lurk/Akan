6e0bf061447229908a5e25dc9c2f8347f6ba277e
nook7466 <zachnjuguna@gmail.com>
2019-09-23 09:09:53 +0300

    add parseInt() function

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let FemaleNames=["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"];
 
 function fetchInputs(){
-  day=document.getElementById(dayID);
-  month=document.getElementById(monthID);
-  year=document.getElementById(yearID);
-  century=document.getElementById(centuryID);
+  day=parseInt(document.getElementById(dayID).value));
+  month=parseInt(document.getElementById(monthID).value));
+  year=parseInt(document.getElementById(yearID).value));
+  century=parseInt(document.getElementById(centuryID);
   validate();
 }
 function validate(){
